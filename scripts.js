7bd35b0b066d3ae0b4e2530c5ca08eabe1ca4b58
beenehdev <beenehdev@gmail.com>
2026-01-05 19:15:26 -0800

    add canvas context and placeholder style

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
+const canvas = document.getElementById("canvasMain");
+const ctx = canvas.getContext('2d');
 
+// Placeholder style for self-demo
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, 480, 320);
+canvas.style.display = 'block'; 
+canvas.style.margin = 'auto';
