30d66e838555057c6970305c97813ba1ed55f92c
beenehdev <beenehdev@gmail.com>
2026-01-07 15:02:28 -0800

    remove clipping from bouncelogic()

--- scripts.js
+++ scripts.js
@@ -16,11 +16,11 @@ function drawBall() {
 }
 
 function bounceLogic() {
-    if (y + dy < 0 || y + dy > canvas.height) {
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
         dy = -dy;
     }
 
-    if (x + dx < 0 || x + dx > canvas.width) {
+    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx;
     }
 }
