8892cb2120606ebea055f194cd6b45758b82519f
beenehdev <beenehdev@gmail.com>
2026-01-07 14:31:31 -0800

    add example functions from mdn tutorial

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
 const canvas = document.getElementById("canvasMain");
 const ctx = canvas.getContext('2d');
 
-// Placeholder style for self-demo
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, 480, 320);
-canvas.style.display = 'block'; 
-canvas.style.margin = 'auto';
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
