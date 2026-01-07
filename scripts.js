bffa521cc2f4cce7ccd410c392a90f773d5ae213
beenehdev <beenehdev@gmail.com>
2026-01-07 15:00:55 -0800

    add bouncelogic() to ball

--- scripts.js
+++ scripts.js
@@ -5,18 +5,30 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
+function bounceLogic() {
+    if (y + dy < 0 || y + dy > canvas.height) {
+        dy = -dy;
+    }
+
+    if (x + dx < 0 || x + dx > canvas.width) {
+        dx = -dx;
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    bounceLogic();
     x += dx;
     y += dy;
 }
